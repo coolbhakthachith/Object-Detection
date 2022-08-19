1f20a679c3f23b3e464d41f394bc702b0ec2bb10
coolbhakthachith <97329201+coolbhakthachith@users.noreply.github.com>
2022-08-19 21:11:33 +0530

    Add files via upload

--- 3.js
+++ 3.js
@@ -1,30 +1,45 @@
-Status = "";
-traffic = "";
+
+img ="";
+status1="";
+objects=[];
 
 function preload(){
-    traffic = loadImage("3.jpg");
+    img= loadImage('3.jpg');
 }
-
 function setup(){
-    canvas = createCanvas(660,350);
-    canvas.center();
-    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    cannvas =createCanvas(640, 420);
+    cannvas.center();
+    objectDetector =ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="STATUS : DECTECTING OBJECT !!!";
 }
 
 function modelLoaded(){
-    console.log("Model Loaded!");
-    Status = true;
-    object_Detector.detect(traffic,gotResults);
+console.log("MODEL LOADED !!!!!!!!!!!");
+status1 =true;
+objectDetector.detect(img,got_result);
 }
 
-function gotResults(error,results){
-    if(error){
-        console.error(error);
-    }
-    console.log(results);
+function got_result(error, results){
+if(error){
+    console.error();
+}
+console.log(results);
+objects =results;
 }
 
 function draw(){
-    image(traffic,0,0,660,350);
+image(img,0,0,640, 420);
+
+if(status1 !=""){
+    for (i =0; i<=objects.length; i++){
+        document.getElementById("status").innerHTML="STATUS :OBJECT DECTECTED !!!";
+    
+    fill("#ff1900");
+    percent = floor(objects[i].confidence * 100);
+    text(objects[i].label+"  "+percent+"%",objects[i].x+15, objects[i].y+15);
+    noFill();
+    stroke("#ff1900");
+    rect(objects[i].x, objects[i].y, objects[i].height, objects[i].width);
+    }
+}
 }
